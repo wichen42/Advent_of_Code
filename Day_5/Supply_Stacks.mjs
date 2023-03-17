6872bffc5e5eabd334b435ba1755ed4ca5279396
Wilson Chen <wilsonchen901@gmail.com>
2023-03-17 13:02:31 -0400

    parse input

--- Day_5/Supply_Stacks.mjs
+++ Day_5/Supply_Stacks.mjs
@@ -1,5 +1,34 @@
 import { readFileSync } from "fs";
 
-const input = readFileSync("./input.txt", {encoding: "utf-8"}).trim().split("\n\n");
+const input = readFileSync("./input.txt", {encoding: "utf-8"}).trimEnd();
 
-console.log(input);
+const [start, moves] = input.split("\n\n");
+
+const parsedStacks = start.split("\n").map((stack) => [...stack].filter((ele, i) => i % 4 === 1));
+const indexes = parsedStacks.pop();
+
+const stacks = {};
+
+for (const stack of parsedStacks) {
+    for (let i = 0; i < stack.length; i++) {
+        if (stack[i] !== " ") {
+            if (!stacks[indexes[i]]) {
+                stacks[indexes[i]] = [];
+            };
+            stacks[indexes[i]].unshift(stack[i]);
+        };
+    };
+};
+
+console.log(parsedStacks);
+
+function part1() {
+
+};
+
+function part2() {
+
+};
+
+part1();
+part2();
