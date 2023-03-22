01db1202390a2e313757913de5bbaa8f6fce58ac
Wilson Chen <wilsonchen901@gmail.com>
2023-03-22 17:16:21 -0400

    complete part 2

--- Day_7/No_Space_Left_On_Device.mjs
+++ Day_7/No_Space_Left_On_Device.mjs
@@ -113,4 +113,35 @@ function part1() {
     console.log(sumSmallFolder);
   }
 
+
+  function part2() {
+    const totalDiskSpace = 70000000;
+    const requiredSpace = 30000000;
+  
+    const tree = createTree(input);
+  
+    const usedSpace = getSize(tree);
+    const availableSpace = totalDiskSpace - usedSpace;
+    if (availableSpace > requiredSpace) {
+      throw new Error("There is already enough space");
+    }
+    const minimumFolderSize = requiredSpace - availableSpace;
+  
+    const candidates = [];
+  
+    getSize(tree, (name, size) => {
+      if (size >= minimumFolderSize) {
+        candidates.push({
+          name,
+          size,
+        });
+      }
+    });
+  
+    candidates.sort((a, b) => a.size - b.size);
+  
+    console.log(candidates[0].size);
+  }
+
 part1();
+part2();
