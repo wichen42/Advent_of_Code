e80cdb8adbde032713a7f8911c23cae9a674a72c
Wilson Chen <wilsonchen901@gmail.com>
2023-03-23 11:55:23 -0400

    parse input

--- Day_8/Treetop_Treehouse.mjs
+++ Day_8/Treetop_Treehouse.mjs
@@ -1,3 +1,5 @@
 import { readFileSync } from 'fs';
 
-const input = readFileSync("./input.txt", {encoding: "utf-8"}).trim()
+const input = readFileSync("./input.txt", {encoding: "utf-8"}).trim().split("\n").map((line) => [...line].map(Number));
+
+console.log(input);
